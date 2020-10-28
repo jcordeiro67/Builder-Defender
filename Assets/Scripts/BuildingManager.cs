42819db7d8aace312332fa939b0859c5e5e99f2f
John Cordeiro <john@johncordeiro.com>
2020-10-28 00:52:20 -0400

    Resource Generation - Pre Challenge

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour {
+
+	private Camera m_Camera;
+	private BuildingTypeSO buildingType;
+	private BuildingTypeListSO buildingTypeList;
+
+	private void Awake ()
+	{
+		//Load the building types from the building type list scriptable Object
+		buildingTypeList = (Resources.Load<BuildingTypeListSO> (typeof (BuildingTypeListSO).Name));
+		//Set default building type
+		buildingType = buildingTypeList.list [0];
+	}
+	private void Start ()
+	{
+		m_Camera = Camera.main;
+
+	}
+
+	private void Update ()
+	{
+		//Place the building type prefab when mouse is clicked
+		if (Input.GetMouseButtonDown (0)) {
+			Instantiate (buildingType.prefab, GetMouseWorldPosition (), Quaternion.identity);
+		}
+
+		//Toggle through building types depending on key pressed
+		if (Input.GetKeyDown (KeyCode.T)) {
+			buildingType = buildingTypeList.list [0];
+		}
+		if (Input.GetKeyDown (KeyCode.Y)) {
+			buildingType = buildingTypeList.list [1];
+		}
+
+	}
+
+	private Vector3 GetMouseWorldPosition ()
+	{
+		//Gets the mouse world position from the camera screen point
+		Vector3 mouseWorldPosition = m_Camera.ScreenToWorldPoint (Input.mousePosition);
+		//Reset Y axis to 0 for mouse position
+		mouseWorldPosition.z = 0f;
+		return mouseWorldPosition;
+	}
+}
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour {
+
+	public static ResourceManager Instance { get; private set; }
+
+
+	private Dictionary<ResourceTypeSO, int> resourceAmountDictionary;
+
+	private void Awake ()
+	{
+		if (Instance == null) {
+			Instance = this;
+		} else {
+			Destroy (gameObject);
+		}
+
+		//Initilize List of Resource Types
+		resourceAmountDictionary = new Dictionary<ResourceTypeSO, int> ();
+		//Populate List with Scriptable Objects from the Scriptable Object List
+		ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO> (typeof (ResourceTypeListSO).Name);
+		//Set default amount for each item in list
+		foreach (ResourceTypeSO resourceType in resourceTypeList.list) {
+			resourceAmountDictionary [resourceType] = 0;
+		}
+
+		TestResourceAmountDictionary ();
+	}
+
+	private void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.T)) {
+			ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO> (typeof (ResourceTypeListSO).Name);
+			AddResource (resourceTypeList.list [0], 2);
+			TestResourceAmountDictionary ();
+		}
+	}
+
+	private void TestResourceAmountDictionary ()
+	{
+		foreach (ResourceTypeSO resourceType in resourceAmountDictionary.Keys) {
+			Debug.Log (resourceType.nameString + "; " + resourceAmountDictionary [resourceType]);
+		}
+	}
+
+	public void AddResource (ResourceTypeSO resourceType, int amount)
+	{
+		resourceAmountDictionary [resourceType] += amount;
+		TestResourceAmountDictionary ();
+	}
+}
